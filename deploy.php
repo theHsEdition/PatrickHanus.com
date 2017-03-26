a0500dbbb0d30c7342f8990a6ace1c8db7798f91
Patrick Hanus <patrick@skymse.com>
2017-03-25 22:00:23 -0400

    Deleted some comments

--- deploy.php
+++ deploy.php
@@ -1,10 +1,6 @@
 <?php
 	/**
 	 * GIT DEPLOYMENT SCRIPT
-	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 *
-	 *		https://gist.github.com/1809044
 	 */
 
 	// The commands
